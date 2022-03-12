727fa7aa8c39e5d2375b0a2cd33506272384d972
yashsrivas <yashsriv@connect.hku.hk>
2022-03-12 13:49:45 +0530

    Prod changes

--- quarantine_dashboard/settings.py
+++ quarantine_dashboard/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f(8)=!$7r8q0q(1)#&v%x5*ascg3a*k52!_d_1&tjf$(n&#@z*'
+SECRET_KEY = env.str('QDD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['afternoon-plains-37625.herokuapp.com','localhost','127.0.0.1']
 
